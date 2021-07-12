016974d395681bfc96ce8113c63b98f74dc1c5c4
Alink97 <alejandro_fg97@icloud.com>
2021-07-12 14:11:46 +0200

    Test de metodo 1 nombre aleatorio

--- src/main/java/ejercicio003/Agenda.java
+++ src/main/java/ejercicio003/Agenda.java
@@ -1,5 +1,27 @@
 package ejercicio003;
 
-public class Agenda {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Agenda {
+	private List<String> lista = new ArrayList<>();
+	
+	public Agenda() {
+		lista.add("Alejandro");
+		lista.add("Bernardo");
+		lista.add("Carlos");
+		lista.add("Daniel");
+		lista.add("Esther");
+		lista.add("Fernando");
+	}
+	
+	public String pedirNombre() {
+		int aleatorio = numAleatorio();
+		return lista.get(aleatorio);
+	}
+	
+	private int numAleatorio() {
+		int numero = (int) Math.floor(Math.random()*lista.size());
+		return numero;
+	}
 }
--- src/test/java/ejercicio003/AgendaTest.java
+++ src/test/java/ejercicio003/AgendaTest.java
@@ -2,18 +2,27 @@ package ejercicio003;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class AgendaTest {
 
+	private Agenda agenda;
+	
 	@BeforeEach
 	void setUp() throws Exception {
+		agenda = new Agenda();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testAleatorio() {
+		List<String> lista = new ArrayList<>();
+		
+		lista.add(agenda.pedirNombre());
+		assertTrue(lista.size() == 1);
 	}
 
 }
